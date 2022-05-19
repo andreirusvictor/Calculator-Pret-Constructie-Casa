84082d0e8821ba1b92c654a924f8be5bf28d9112
andre <andreirusvictor@yahoo.ro>
2022-05-19 09:46:50 +0300

    update 1

--- calculator.py
+++ calculator.py
@@ -5,12 +5,12 @@ app = Flask('calculator')
 
 @app.route('/first_page')
 def first_page():
-    return render_template('first_page.html')
+    return render_template('first_page.html', title='Formular de calcul')
 
 
 @app.route('/second_page')
 def second_page():
-    return render_template('second_page.html')
+    return render_template('second_page.html', title='Detaliere Costuri')
 
 
 if __name__ == '__main__':
