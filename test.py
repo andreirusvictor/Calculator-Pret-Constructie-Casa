9ac23447cfd1865b4b49f42bd1868a7c056996f7
andre <andreirusvictor@yahoo.ro>
2022-06-05 14:24:58 +0300

    update 1

--- test.py
+++
@@ -1,7 +0,0 @@
-# dict(id=row[0], descriere=row[1], valoare=row[2])
-# {'id': row[0], 'descriere': row[1], 'valoare': row[2]}
-
-for letter in 'Python':
-   if letter == 'h':
-      continue
-   print 'Current Letter :', letter
